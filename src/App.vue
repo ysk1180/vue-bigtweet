50a9dcff7c013a4cd259cc186378a77ce161452d
Yusuke Miyazaki <ysk1180@gmail.com>
2019-04-27 20:55:20 +0900

    css追加

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
-    <input type="radio" id="black" value="black" v-model="image">
-    <label for="black"><img src="./assets/black.png"></label>
-    <input type="radio" id="red" value="red" v-model="image">
-    <label for="red"><img src="./assets/red.png"></label>
-
-    <textarea v-model="msg" placeholder="ここに入力してください"></textarea>
-    <p>
-      {{ msg }}
-    </p>
-    <img :src="image_path(image)" />
+    <div>
+      <input type="radio" id="black" value="black" v-model="image">
+      <label for="black"><img src="./assets/black.png"></label>
+      <input type="radio" id="red" value="red" v-model="image">
+      <label for="red"><img src="./assets/red.png"></label>
+    </div>
+    <div>
+      <textarea v-model="msg" placeholder="ここに入力してください"></textarea>
+    </div>
+    <div id="preview">
+      <img :src="image_path(image)" />
+      <p>{{ msg }}</p>
+    </div>
   </div>
 </template>
 
@@ -30,4 +33,27 @@ export default {
 </script>
 
 <style>
+#preview {
+  width: 315px;
+  text-align: center;
+  display: inline-block;
+  margin-bottom: 20px;
+  position: relative;
+}
+#preview p {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -ms-transform: translate(-50%,-50%);
+  -webkit-transform: translate(-50%,-50%);
+  transform: translate(-50%,-50%);
+  margin:0;
+  padding:0;
+  font-size: 35px;
+  color: white;
+  white-space: pre;
+  line-height: 1em;
+  text-align: initial;
+  font-weight: bold;
+}
 </style>
